bb6c43fda638a95e6c12f452cad766ae1e01af45
Taiwo48 <adebayotaiwo2007@gmail.com>
2024-10-24 09:47:33 +0100

    semicolon-assignment

---
+++ Python class/feet.py
@@ -0,0 +1,4 @@
+feet = int(input('Enter a number for feet:'))
+
+meter = feet * 0.305
+print(" feet is " , meter , " meters")
---
+++ Python class/weight.py
@@ -0,0 +1,8 @@
+height = int(input("Enter height in inches:"))
+weight  = int(input("Enter weight in pounds:"))
+
+
+
+bmi =( weight * weight) / (height * height)
+
+print("The BMI is ",bmi)
