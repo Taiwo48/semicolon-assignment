b32a64f21d2e63a678cc14ba70980354cc0a3030
Taiwo48 <adebayotaiwo2007@gmail.com>
2024-10-28 00:28:37 +0100

    semicolon-assignment

---
+++ java class/Pattern.java
@@ -0,0 +1,7 @@
+public class Pattern{
+  public static void main(String[] args){
+ System.out.print("pattern A    pattern B    pattern C    pattern D");
+ System.out.println("\n*            * * * * * *          *    * * * * * *\n* *          * * * * *          * *      * * * * *\n* * *        * * * *          * * *        * * * *\n* * * *      * * *          * * * *          * * *\n* * * * *    * *          * * * * *            * *\n* * * * * *  *          * * * * * *              *");
+ 
+}
+}
