9bbd3876bfc91c6497ea0884a7bb7948dad6c03c
Taiwo48 <adebayotaiwo2007@gmail.com>
2024-11-01 00:53:35 +0100

    semicolon-assignment

---
+++ Python class/increasingordecreasing.py
@@ -0,0 +1,11 @@
+number1 = int(input('Enter first number: '))
+
+number2 = int(input('Enter second number: '))
+
+number3 = int(input('Enter third number: '))
+
+if number1 > number2 and number2 > number3:
+	print('decreasing order')
+if number1 < number2 and number2 < number3:
+	print('increasing order')
+
